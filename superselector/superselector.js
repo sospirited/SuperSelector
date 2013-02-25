0db510c539b94bbe31402e2d5236d53b05173b41
Pre <priyankv@gmail.com>
2013-02-25 13:45:32 +0000

    updating styling for selected element

--- superselector/superselector.js
+++ superselector/superselector.js
@@ -43,7 +43,7 @@
 		}
 		this.prevElement = target;
 		this.prevElementBorder = target.style.outline;
-		target.style.outline = "2px black dashed";
+		target.style.outline = "1px black dashed";
 	};
 
 	superselector.prototype.configOptionIsSelected = function(sConfigId)
